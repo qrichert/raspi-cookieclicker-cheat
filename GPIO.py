282cb34357c4fc3a4c783c56e6fc4f16c409da29
Quentin Richert <quentin@quentinrichert.com>
2020-01-21 12:26:17 +0100

    Intial commit.

---
+++ GPIO.py
@@ -0,0 +1,33 @@
+"""
+Placeholder for Rpi.GPIO to test on regular computer
+"""
+
+
+class GPIO:
+	BOARD = 'board'
+	BCM = 'broadcom'
+	IN = 'input'
+	OUT = 'output'
+	HIGH = True
+	LOW = False
+
+	@staticmethod
+	def setmode(_):
+		pass
+
+	@staticmethod
+	def setwarnings(_):
+		pass
+
+	@staticmethod
+	def setup(_, __, initial):
+		pass
+
+	@staticmethod
+	def input(_):
+		pass
+
+	@staticmethod
+	def output(_, state):
+		# print('HIGH' if state else 'LOW')
+		pass
---
+++ README.md
@@ -0,0 +1,10 @@
+Raspberry Pi Cookie Clicker 2 Cheat
+===================================
+
+Cheating utility for Cookie Clicker 2 using a Raspberry Pi's GPIO interface.
+
+<p align="center">
+    <a href="#" target="_blank">
+        <img src="sw__interface.png" alt="Terminal Window" width="283">
+    </a>
+</p>
